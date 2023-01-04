198a4a9b417c3e0692a45756d6603424ba52b4e1
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-01-03 23:20:42 -0500

    added block content

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const tabTwo = document.querySelector('.two');
 const tabThree = document.querySelector('.three');
 const tabArray = [tabOne,tabTwo,tabThree];
 
+const menuBlock = document.querySelector('.menuBlock');
+const infoBlock = document.querySelector('.infoBlock');
+const blockArray = [infoBlock,menuBlock];
+
 for(let i = 0; i<info.length; i++){
     hideTip(info[i]);
 }
@@ -24,6 +28,11 @@ for(let i = 0; i< buttonArray.length;i++){
 for(let i = 0; i< tabArray.length;i++){
     tabArray[i].addEventListener('click', () =>{
         revealTip(info[buttonArray.length+i])
+    })
+}
+for(let i = 0; i< blockArray.length;i++){
+    blockArray[i].addEventListener('click', () =>{
+        revealTip(info[blockArray.length + tabArray.length + i + 2])
     })
 }
 for(let i=0 ;i< closeInfo.length;i++){
@@ -46,7 +55,7 @@ function hideTip(item){
         item.style.width = '0';
         item.style.height = '0';
         item.style.opacity = '0';
-        item.style.right = '10rem';
+        item.style.right = '10rem'; //* causes overflow
         item.style.top = '0';
         item.style.transition = 'opacity 0s ease-in';
 }
