9f796fc2310cc7343296b15349a2757f2d7a752f
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-01-03 21:20:51 -0500

    added tabs info, favicon

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 const info = document.querySelectorAll('.info');
 const closeInfo = document.querySelectorAll('.close');
+const infoPara = document.querySelectorAll('.info code, .info .sug')
+
 const trash = document.querySelector('.trash');
 const add = document.querySelector('.add');
 const claim = document.querySelector('.claim');
 const submit = document.querySelector('.submit');
-const buttonItemArray = [trash,add,claim,submit];
+const buttonArray = [trash,add,claim,submit];
+
+const tabOne = document.querySelector('.one');
+const tabTwo = document.querySelector('.two');
+const tabThree = document.querySelector('.three');
+const tabArray = [tabOne,tabTwo,tabThree];
 
 for(let i = 0; i<info.length; i++){
-    info[i].style.display = 'none';
+    hideTip(info[i]);
 }
-for(let i = 0; i< buttonItemArray.length;i++){
-    buttonItemArray[i].addEventListener('click', () =>{
-        info[i].style.display= 'block'
+for(let i = 0; i< buttonArray.length;i++){
+    buttonArray[i].addEventListener('click', () =>{
+        revealTip(info[i])
+    })
+}
+for(let i = 0; i< tabArray.length;i++){
+    tabArray[i].addEventListener('click', () =>{
+        revealTip(info[buttonArray.length+i])
     })
 }
 for(let i=0 ;i< closeInfo.length;i++){
     closeInfo[i].addEventListener('click', () =>{
         for(let j = 0; j < info.length;j++){
-            info[j].style.display = "none";
+            hideTip(info[j]);
         }
     })
+}
+
+function revealTip(item){
+        item.style.width = '320px';
+        item.style.height = 'fit-content';
+        item.style.right = 'auto';
+        item.style.top = '0';
+        item.style.opacity = '1';
+        item.style.transition = 'opacity .25s ease-in';
+}
+function hideTip(item){
+        item.style.width = '0';
+        item.style.height = '0';
+        item.style.opacity = '0';
+        item.style.right = '10rem';
+        item.style.top = '0';
+        item.style.transition = 'opacity 0s ease-in';
 }
