414156c92d34ac7387213727128a0ae51337bf65
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-01-04 22:35:13 -0500

    added to block section, fixed ux problem

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const tabThree = document.querySelector('.three');
 const tabArray = [tabOne,tabTwo,tabThree];
 
 const menuBlock = document.querySelector('.menuBlock');
+const blurBlock = document.querySelector('.blurBlock');
 const infoBlock = document.querySelector('.infoBlock');
-const blockArray = [infoBlock,menuBlock];
+const blockArray = [infoBlock,blurBlock,menuBlock];
 
 for(let i = 0; i<info.length; i++){
     hideTip(info[i]);
@@ -32,7 +33,7 @@ for(let i = 0; i< tabArray.length;i++){
 }
 for(let i = 0; i< blockArray.length;i++){
     blockArray[i].addEventListener('click', () =>{
-        revealTip(info[blockArray.length + tabArray.length + i + 2])
+        revealTip(info[blockArray.length + tabArray.length + i + 1])
     })
 }
 for(let i=0 ;i< closeInfo.length;i++){
@@ -55,7 +56,6 @@ function hideTip(item){
         item.style.width = '0';
         item.style.height = '0';
         item.style.opacity = '0';
-        item.style.right = '10rem'; //* causes overflow
-        item.style.top = '0';
+        item.style.top = '-100rem';
         item.style.transition = 'opacity 0s ease-in';
 }
